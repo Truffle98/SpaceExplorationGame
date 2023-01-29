61a335337a9337b3151601620f2bc5490e1e182a
Truffle98 <75415271+Truffle98@users.noreply.github.com>
2023-01-29 05:15:14 +0000

    Added basics for vision system

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public Camera cam;
-    const float moveSpeed = 0.25f;
+    public float speed;
+    private Rigidbody2D body;
+    private List<GameObject> newVisible = new List<GameObject>(), oldVisible = new List<GameObject>();
 
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
@@ -26,35 +28,65 @@ public class PlayerScript : MonoBehaviour
         transform.up = direction;
         float angle = Mathf.Atan2(direction.y, direction.x);
         
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
-        if (hit.collider != null)
+        float lineDistance = 10;
+        float spread = 0.075f;
+        int numLines = 13;
+
+        oldVisible.Clear();
+        foreach (GameObject go in newVisible)
         {
-            Debug.Log(hit.collider.gameObject);
+            oldVisible.Add(go);
         }
+        newVisible.Clear();
+
+        for (int i = 0; i < numLines; i++)
+        {
+            float adjust = (i * spread) - (spread * (numLines / 2));
+            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, new Vector2(lineDistance * Mathf.Cos(angle + adjust), lineDistance * Mathf.Sin(angle + adjust)), 10);
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.collider.gameObject != gameObject)
+                {
+                    if (hit.collider.tag == "Wall")
+                    {
+                        break;
+                    }
+
+                    if (!newVisible.Contains(hit.collider.gameObject))
+                    {
+                        //Debug.Log("Can see: " + hit.collider.gameObject);
+                        newVisible.Add(hit.collider.gameObject);
+                    }
+                }
+            }
+
+            foreach (GameObject go in oldVisible)
+            {
+                if (!newVisible.Contains(go))
+                {
+                    go.GetComponent<SpriteRenderer>().enabled = false;
+                }
+            }
+
+            foreach (GameObject go in newVisible)
+            {
+                //if (!oldVisible.Contains(go))
+                //{
+                go.GetComponent<SpriteRenderer>().enabled = true;
+                //}
+            }
+
+            Vector2 rayLine = new Vector2(lineDistance * Mathf.Cos(angle + adjust), lineDistance * Mathf.Sin(angle + adjust));
+            Debug.DrawRay(transform.position, rayLine, Color.red);
+        }
+        
 
     }
 
     void MovePlayer()
     {
-        if (Input.GetKey("w"))
-        {
-            transform.position += new Vector3(0, moveSpeed, 0);
-        }
-
-        if (Input.GetKey("s"))
-        {
-            transform.position += new Vector3(0, -moveSpeed, 0);
-        }
-
-        if (Input.GetKey("d"))
-        {
-            transform.position += new Vector3(moveSpeed, 0, 0);
-        }
-
-        if (Input.GetKey("a"))
-        {
-            transform.position += new Vector3(-moveSpeed, 0, 0);
-        }
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
     }
 
 }
