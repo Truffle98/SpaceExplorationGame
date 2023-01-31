e4c3465aabe2cb0b7e480dbbe32b6883a267344b
j03m4r <joeemarr12345@gmail.com>
2023-01-31 00:12:54 -0600

    Cleaned up the player script and flashlight scripts (removed some unnecessary conditionals and loops). Made flashlight anim. Made external light sources actually display objects when the objects affected by the light are in FOV

---
+++ Assets/Scripts/LanternScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternScript : MonoBehaviour
+{
+    public float visionLength = 2f;
+    private float spread;
+    private int numLines;
+    void Start()
+    {
+        numLines = 25; // 25 raycasting lines
+        spread = 2f * Mathf.PI / numLines; // Defines the spread between each ray (evenly spaced over 2PI by 25 lines)
+        for (int i = 0; i < numLines; i++)
+        {
+            Vector2 rayDirection = new Vector2(Mathf.Cos(spread*i), Mathf.Sin(spread*i));
+            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, rayDirection, visionLength);
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.collider.gameObject != gameObject) // Make sure the object we detect isn't the player
+                {
+                    if (hit.collider.tag == "Wall")
+                    {
+                        break;
+                    }
+                    
+                    hit.collider.gameObject.GetComponent<VisibilityScript>().isVisible = true;
+                }
+            }
+
+            Vector2 rayLine = new Vector2(visionLength * Mathf.Cos(spread*i), visionLength * Mathf.Sin(spread*i));
+            Debug.DrawRay(transform.position, rayLine, Color.red);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        raycast();
+    }
+
+    void raycast()
+    {
+        for (int i = 0; i < numLines; i++)
+        {
+            Vector2 rayDirection = new Vector2(Mathf.Cos(spread*i), Mathf.Sin(spread*i));
+            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, rayDirection, visionLength);
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.collider.gameObject != gameObject) // Make sure the object we detect isn't the player
+                {
+                    if (hit.collider.tag == "Wall")
+                    {
+                        break;
+                    }
+                    
+                    VisibilityScript vs = hit.collider.gameObject.GetComponent<VisibilityScript>();
+
+                    if (vs != null)
+                    {
+                        hit.collider.gameObject.GetComponent<VisibilityScript>().isVisible = true;
+                    }
+                }
+            }
+
+            Vector2 rayLine = new Vector2(visionLength * Mathf.Cos(spread*i), visionLength * Mathf.Sin(spread*i));
+            Debug.DrawRay(transform.position, rayLine, Color.red);
+        }
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public Camera cam;
-    public float speed;
+    public float speed, angle;
     private float playerRotation = Mathf.PI / 2f;
     private Rigidbody2D body;
-    private List<GameObject> newVisible = new List<GameObject>(), oldVisible = new List<GameObject>();
+    private List<GameObject> newVisible = new List<GameObject>(), oldVisible = new List<GameObject>(), 
+    inVision = new List<GameObject>(), oldInVision = new List<GameObject>();
 
     void Start()
     {
@@ -18,12 +19,15 @@ public class PlayerScript : MonoBehaviour
     void FixedUpdate()
     {
         TurnPlayer();
+        RaycastVision();
         MovePlayer();
         cam.transform.position = transform.position - new Vector3 (0, 0, 10);
     }
 
     void TurnPlayer()
     {
+        // These variables/section primarily deal with rotating the player. We find the mouse's world position and
+        // rotate the player to the position angle
         Vector2 mouseScreenPosition = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = (mouseScreenPosition - (Vector2) transform.position).normalized;
         float desiredAngle = Mathf.Atan2(direction.y, direction.x);
@@ -37,7 +41,7 @@ public class PlayerScript : MonoBehaviour
             desiredAngle += Mathf.PI * 2;
         }
 
-        float angle = (desiredAngle - playerRotation) * 0.1f + playerRotation;
+        angle = (desiredAngle - playerRotation) * 0.1f + playerRotation;
 
         if (angle > Mathf.PI)
         {
@@ -50,11 +54,14 @@ public class PlayerScript : MonoBehaviour
 
         playerRotation = angle;
         transform.up = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-        
-        float lineDistance = 15;
-        float spread = 0.05f;
-        int numLines = 18;
 
+        // Local vision raycasting section
+        int numLines = 25; // 25 raycasting lines
+        float lineDistance = 2f; // How far local vision extends (2 units)
+        float leftAngle = angle + Mathf.PI / 6f; // Left angle represents where we should start drawing the rays
+        float spread = 2f * Mathf.PI / numLines; // Defines the spread between each ray (evenly spaced over 2PI by 25 lines)
+
+        // Updating the oldVisible array
         oldVisible.Clear();
         foreach (GameObject go in newVisible)
         {
@@ -62,6 +69,55 @@ public class PlayerScript : MonoBehaviour
         }
         newVisible.Clear();
 
+        // Creating rays for local vision and changing visibility on objects appropriately
+        for (int i = 0; i < numLines; i++)
+        {
+            Vector2 rayDirection = new Vector2(Mathf.Cos(leftAngle+spread*i), Mathf.Sin(leftAngle+spread*i));
+            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, rayDirection, lineDistance);
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.collider.gameObject != gameObject) // Make sure the object we detect isn't the player
+                {
+                    if (hit.collider.tag == "Wall")
+                    {
+                        break;
+                    }
+
+                    newVisible.Add(hit.collider.gameObject);
+                    if (!oldVisible.Contains(hit.collider.gameObject))
+                    {
+                        hit.collider.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                    }
+                }
+            }
+
+            Vector2 rayLine = new Vector2(lineDistance * Mathf.Cos(leftAngle+spread*i), lineDistance * Mathf.Sin(leftAngle+spread*i));
+            Debug.DrawRay(transform.position, rayLine, Color.red);
+        }
+
+        // Disable no longer visible objects
+        foreach (GameObject go in oldVisible)
+        {
+            if (!newVisible.Contains(go))
+            {
+                go.GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
+    }
+
+    void MovePlayer()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+    }
+
+    void RaycastVision()
+    {
+        float lineDistance = 50;
+        float spread = 0.025f;
+        int numLines = 25;
+
+        inVision.Clear();
         for (int i = 0; i < numLines; i++)
         {
             float adjust = (i * spread) - (spread * (numLines / 2));
@@ -75,98 +131,36 @@ public class PlayerScript : MonoBehaviour
                     if (hit.collider.tag == "Wall")
                     {
                         break;
-                    }
-
-                    if (!newVisible.Contains(hit.collider.gameObject))
-                    {
-                        //Debug.Log("Can see: " + hit.collider.gameObject);
-                        newVisible.Add(hit.collider.gameObject);
-                    }
-                }
-            }
-
-            Vector2 rayLine = new Vector2(lineDistance * Mathf.Cos(angle + adjust), lineDistance * Mathf.Sin(angle + adjust));
-            Debug.DrawRay(transform.position, rayLine, Color.red);
-        }
-
-        // Ray casting for behind
-        numLines = 25;
-        lineDistance = 2f;
-        float leftAngle = angle + Mathf.PI / 6f;
-        spread = 2f * Mathf.PI / numLines;
-        for (int i = 0; i < numLines; i++)
-        {
-            Vector2 rayDirection = new Vector2(Mathf.Cos(leftAngle+spread*i), Mathf.Sin(leftAngle+spread*i));
-            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, rayDirection, lineDistance);
-
-            foreach (RaycastHit2D hit in hits)
-            {
-                if (hit.collider.gameObject != gameObject)
-                {
-                    if (hit.collider.tag == "Wall")
-                    {
-                        break;
-                    }
-
-                    if (!newVisible.Contains(hit.collider.gameObject))
-                    {
-                        //Debug.Log("Can see: " + hit.collider.gameObject);
-                        newVisible.Add(hit.collider.gameObject);
+                    } else {
+                        VisibilityScript vs = hit.collider.gameObject.GetComponent<VisibilityScript>();
+                        if (vs != null)
+                        {
+                            inVision.Add(hit.collider.gameObject);
+                            if (vs.isVisible)
+                            {
+                                hit.collider.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                            }
+                        }
                     }
                 }
             }
-
-            Vector2 rayLine = new Vector2(lineDistance * Mathf.Cos(leftAngle+spread*i), lineDistance * Mathf.Sin(leftAngle+spread*i));
-            Debug.DrawRay(transform.position, rayLine, Color.red);
-        }
-        // Debug.DrawRay(transform.position, new Vector2(lineDistance * Mathf.Cos(leftAngle), lineDistance * Mathf.Sin(leftAngle)), Color.green);
-        // for (float i = 0; i <= leftAngle+Mathf.PI; i+=Mathf.PI/12)
-        // {
-        //     Vector2 rayDirection = new Vector2(lineDistance / 4 * Mathf.Cos(leftAngle+i), lineDistance / 4 * Mathf.Sin(leftAngle+i));
-        //     RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, rayDirection, lineDistance / 4);
-
-        //     foreach (RaycastHit2D hit in hits)
-        //     {
-        //         if (hit.collider.gameObject != gameObject)
-        //         {
-        //             if (hit.collider.tag == "Wall")
-        //             {
-        //                 break;
-        //             }
-
-        //             if (!newVisible.Contains(hit.collider.gameObject))
-        //             {
-        //                 //Debug.Log("Can see: " + hit.collider.gameObject);
-        //                 newVisible.Add(hit.collider.gameObject);
-        //             }
-        //         }
-        //     }
             
-        //     Vector2 rayLine = new Vector2(lineDistance/4 * Mathf.Cos(leftAngle+i), lineDistance/4 * Mathf.Sin(leftAngle+i));
-        //     Debug.DrawRay(transform.position, rayLine, Color.red);
-        // }
+            Vector2 rayLine = new Vector2(lineDistance * Mathf.Cos(angle+adjust), lineDistance * Mathf.Sin(angle+adjust));
+            Debug.DrawRay(transform.position, rayLine, Color.red);
+        }
 
-        foreach (GameObject go in oldVisible)
+        foreach (GameObject go in oldInVision)
         {
-            if (!newVisible.Contains(go))
+            if (!inVision.Contains(go))
             {
                 go.GetComponent<SpriteRenderer>().enabled = false;
             }
         }
 
-        foreach (GameObject go in newVisible)
+        foreach (GameObject go in inVision)
         {
-            if (!oldVisible.Contains(go))
-            {
-                go.GetComponent<SpriteRenderer>().enabled = true;
-            }
+            oldInVision.Add(go);
         }
-
-    }
-
-    void MovePlayer()
-    {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
     }
 
 }
