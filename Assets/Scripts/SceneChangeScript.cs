4f1b2c3d286a17d10fae4f43a745602c60e2aa22
Truffle98 <75415271+Truffle98@users.noreply.github.com>
2023-02-02 03:14:35 +0000

    City tilemap basic generation

--- Assets/Scripts/SceneChangeScript.cs
+++ Assets/Scripts/SceneChangeScript.cs
@@ -13,6 +13,7 @@ public class SceneChangeScript : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            other.gameObject.GetComponent<PlayerScript>().MovingRooms();
             other.gameObject.transform.position = new Vector3(newPlayerLocation.x, newPlayerLocation.y, 0);
             SceneManager.LoadScene(newSceneName);
         }
